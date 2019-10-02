c5b518f44492439e767fa1e2b6e7f24b32e3c16d
Shiyue Cheng <shiychen@cisco.com>
2019-10-02 09:47:20 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'meraki',
+  packages = ['meraki'],
+  version = '0.34',
+  description = 'Cisco Meraki Dashboard API Python 3.6 Module',
+  author = 'Shiyue Cheng',
+  author_email = 'shiychen@cisco.com',
+  url = 'https://github.com/meraki/dashboard-api-python',
+  download_url = 'https://github.com/meraki/dashboard-api-python/',
+  keywords = ['meraki', 'dashboard', 'cisco'],
+  classifiers = [],
+)
