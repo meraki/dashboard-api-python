97f375ac4fb32d900aed51eb22ff930412054d58
Pete Johnson <petecj2@gmail.com>
2020-05-04 14:17:50 -0400

    Placed extended user agent code in separate function and streamlined it per PR comment

--- meraki_v1/rest_session.py
+++ meraki_v1/rest_session.py
@@ -10,6 +10,46 @@ import platform
 from .config import *
 from .exceptions import *
 
+def user_agent_extended(be_geo_id, caller):
+    # Generate extended portion of the User-Agent
+    user_agent_extended = be_geo_id
+    user_agent_extended = {}
+
+    # Mimic pip system data collection per https://github.com/pypa/pip/blob/master/src/pip/_internal/network/session.py
+    user_agent_extended['implementation'] = {
+            "name": platform.python_implementation(),
+        }
+
+    if user_agent_extended["implementation"]["name"] in ('CPython','Jython','IronPython'):
+        user_agent_extended["implementation"]["version"] = platform.python_version()
+    elif user_agent_extended["implementation"]["name"] == 'PyPy':
+        if sys.pypy_version_info.releaselevel == 'final':
+            pypy_version_info = sys.pypy_version_info[:3]
+        else:
+            pypy_version_info = sys.pypy_version_info
+        user_agent_extended["implementation"]["version"] = ".".join(
+            [str(x) for x in pypy_version_info]
+        )
+
+    if sys.platform.startswith("darwin") and platform.mac_ver()[0]:
+        user_agent_extended["distro"] = {"name": "macOS", "version": platform.mac_ver()[0]}
+
+    if platform.system():
+        user_agent_extended.setdefault("system", {})["name"] = platform.system()
+
+    if platform.release():
+        user_agent_extended.setdefault("system", {})["release"] = platform.release()
+
+    if platform.machine():
+        user_agent_extended["cpu"] = platform.machine()
+
+    if be_geo_id:
+        user_agent_extended["be_geo_id"] = be_geo_id
+
+    if caller:
+        user_agent_extended["caller"] = caller
+
+    return urllib.parse.quote(json.dumps(user_agent_extended))
 
 # Main module interface
 class RestSession(object):
@@ -57,53 +97,11 @@ class RestSession(object):
         elif self._base_url[-1] == '/':
             self._base_url = self._base_url[:-1]
 
-        # Generate extended portion of the User-Agent
-        user_agent_extended = be_geo_id
-        user_agent_extended = {}
-
-        # Mimic pip system data collection per https://github.com/pypa/pip/blob/master/src/pip/_internal/network/session.py
-        user_agent_extended['implementation'] = {
-                "name": platform.python_implementation(),
-            }
-
-        if user_agent_extended["implementation"]["name"] == 'CPython':
-            user_agent_extended["implementation"]["version"] = platform.python_version()
-        elif user_agent_extended["implementation"]["name"] == 'PyPy':
-            if sys.pypy_version_info.releaselevel == 'final':
-                pypy_version_info = sys.pypy_version_info[:3]
-            else:
-                pypy_version_info = sys.pypy_version_info
-            user_agent_extended["implementation"]["version"] = ".".join(
-                [str(x) for x in pypy_version_info]
-            )
-        elif user_agent_extended["implementation"]["name"] == 'Jython':
-            user_agent_extended["implementation"]["version"] = platform.python_version()
-        elif user_agent_extended["implementation"]["name"] == 'IronPython':
-            user_agent_extended["implementation"]["version"] = platform.python_version()
-
-        if sys.platform.startswith("darwin") and platform.mac_ver()[0]:
-            user_agent_extended["distro"] = {"name": "macOS", "version": platform.mac_ver()[0]}
-
-        if platform.system():
-            user_agent_extended.setdefault("system", {})["name"] = platform.system()
-
-        if platform.release():
-            user_agent_extended.setdefault("system", {})["release"] = platform.release()
-
-        if platform.machine():
-            user_agent_extended["cpu"] = platform.machine()
-
-        if be_geo_id:
-            user_agent_extended["be_geo_id"] = be_geo_id
-
-        if caller:
-            user_agent_extended["caller"] = caller
-
         # Update the headers for the session
         self._req_session.headers = {
             'Authorization': 'Bearer ' + self._api_key,
             'Content-Type': 'application/json',
-            'User-Agent': 'python-meraki/1.0.0b1 ' + urllib.parse.quote(json.dumps(user_agent_extended)),
+            'User-Agent': 'python-meraki/1.0.0b1 ' + user_agent_extended(be_geo_id, caller),
         }
 
         # Log API calls
