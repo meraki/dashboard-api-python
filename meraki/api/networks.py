79fd08198cad8f5725429ca00f03e40c86bfb840
wyw <wyw@exands.com>
2021-01-28 08:58:00 +0800

    fix bind network template autoBind problem

--- meraki/api/networks.py
+++ meraki/api/networks.py
@@ -176,7 +176,7 @@ class Networks(object):
         
         - networkId (string): (required)
         - configTemplateId (string): The ID of the template to which the network should be bound.
-        - autoBind (boolean): Optional boolean indicating whether the network's switches should automatically bind to profiles of the same model. Defaults to false if left unspecified. This option only affects switch networks and switch templates. Auto-bind is not valid unless the switch template has at least one profile and has at most one profile per switch model.
+        - autoBind (string): 'true' or 'false', optional string indicating whether the network's switches should automatically bind to profiles of the same model. Defaults to false if left unspecified. This option only affects switch networks and switch templates. Auto-bind is not valid unless the switch template has at least one profile and has at most one profile per switch model.
         """
 
         kwargs.update(locals())
