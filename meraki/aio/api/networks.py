7a0ea1ccd2903d484ac778be1fe8e498c12d76de
GitHub Action <support@meraki.com>
2023-12-08 09:00:32 +0000

    Automatically regenerated library.

--- meraki/aio/api/networks.py
+++ meraki/aio/api/networks.py
@@ -246,7 +246,7 @@ class AsyncNetworks:
         - direction (string): direction to paginate, either "next" (default) or "prev" page
         - t0 (string): The beginning of the timespan for the data. The maximum lookback period is 31 days from today.
         - timespan (number): The timespan for which the information will be fetched. If specifying timespan, do not specify parameter t0. The value must be in seconds and be less than or equal to 31 days. The default is 1 day.
-        - perPage (integer): The number of entries per page returned. Acceptable range is 3 - 1000. Default is 10.
+        - perPage (integer): The number of entries per page returned. Acceptable range is 3 - 5000. Default is 10.
         - startingAfter (string): A token used by the server to indicate the start of the page. Often this is a timestamp or an ID but it is not limited to those. This parameter should not be defined by client applications. The link for the first, last, prev, or next page in the HTTP Link header should define it.
         - endingBefore (string): A token used by the server to indicate the end of the page. Often this is a timestamp or an ID but it is not limited to those. This parameter should not be defined by client applications. The link for the first, last, prev, or next page in the HTTP Link header should define it.
         - statuses (array): Filters clients based on status. Can be one of 'Online' or 'Offline'.
