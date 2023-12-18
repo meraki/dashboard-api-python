7869ca8b9ba0d67e5d0f7051fab39080653d621f
John M. Kuchta <git@chir.us>
2023-12-18 10:28:18 -0800

    Automatically regenerated library. (#237)
    Co-authored-by: GitHub Action <support@meraki.com>
    
    Includes mid-cycle release 1.41.0.

--- meraki/api/batch/appliance.py
+++ meraki/api/batch/appliance.py
@@ -912,7 +912,7 @@ class ActionBatchAppliance(object):
         - networkId (string): Network ID
         - enabled (boolean): Boolean value to enable or disable the BGP configuration. When BGP is enabled, the asNumber (ASN) will be autopopulated with the preconfigured ASN at other Hubs or a default value if there is no ASN configured.
         - asNumber (integer): An Autonomous System Number (ASN) is required if you are to run BGP and peer with another BGP Speaker outside of the Auto VPN domain. This ASN will be applied to the entire Auto VPN domain. The entire 4-byte ASN range is supported. So, the ASN must be an integer between 1 and 4294967295. When absent, this field is not updated. If no value exists then it defaults to 64512.
-        - ibgpHoldTimer (integer): The IBGP holdtimer in seconds. The IBGP holdtimer must be an integer between 12 and 240. When absent, this field is not updated. If no value exists then it defaults to 240.
+        - ibgpHoldTimer (integer): The iBGP holdtimer in seconds. The iBGP holdtimer must be an integer between 12 and 240. When absent, this field is not updated. If no value exists then it defaults to 240.
         - neighbors (array): List of BGP neighbors. This list replaces the existing set of neighbors. When absent, this field is not updated.
         """
 
