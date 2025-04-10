94d24a51b76f87a9c97ea0661f061bbb6757362d
John M. Kuchta <git@chir.us>
2025-04-09 20:46:43 -0700

    Extractathon!
    * Extracting parts of rest_session.py into common.py
    * Creating input validation for caller and be_geo_id arguments
    * Improving help text on config.py

--- meraki/common.py
+++ meraki/common.py
@@ -1,5 +1,8 @@
 import platform
 from meraki.exceptions import *
+import re
+import sys
+import urllib.parse
 
 
 def check_python_version():
@@ -20,3 +23,63 @@ def check_python_version():
         )
 
         raise PythonVersionError(message)
+
+
+def validate_user_agent(be_geo_id, caller):
+    # Generate extended portion of the User Agent
+    # Validate that it follows the expected format
+    user_agent = dict()
+
+    allowed_format_in_regex = r'^[A-Za-z0-9]+(?:/[0-9A-Za-z]+(?:\.[0-9A-Za-z]+)*(-[a-z]+)?)? [A-Za-z-0-9]+$'
+
+    if caller and re.match(allowed_format_in_regex, caller):
+        user_agent["caller"] = caller
+    elif be_geo_id and re.match(allowed_format_in_regex, be_geo_id):
+        user_agent["caller"] = be_geo_id
+    else:
+        if caller:
+            message = "Please follow the user agent format prescribed in our User Agents guide, available here:"
+            doc_link = "https://developer.cisco.com/meraki/api-v1/user-agents-overview/"
+            raise SessionInputError("MERAKI_PTYHON_SDK_CALLER", caller, message, doc_link)
+        elif be_geo_id:
+            message = "Use of be_geo_id is deprecated. Please use the argument MERAKI_PTYHON_SDK_CALLER instead."
+            doc_link = "https://developer.cisco.com/meraki/api-v1/user-agents-overview/"
+            raise SessionInputError("BE_GEO_ID", caller, message, doc_link)
+        else:
+            user_agent["caller"] = "unidentified"
+
+    caller_string = f'Caller/({user_agent["caller"]})'
+
+    return caller_string
+
+
+def reject_v0_base_url(self):
+    if 'v0' in self._base_url:
+        sys.exit(f'This library does not support dashboard API v0 ({self._base_url} was configured as the base'
+                 f' URL).  API v0 has been end of life since 2020 August 5.')
+    elif self._base_url[-1] == '/':
+        self._base_url = self._base_url[:-1]
+
+
+def iterator_for_get_pages_bool(self):
+    return self._use_iterator_for_get_pages
+
+
+def use_iterator_for_get_pages_setter(self, value):
+    if value:
+        self.get_pages = self._get_pages_iterator
+    else:
+        self.get_pages = self._get_pages_legacy
+
+    self._use_iterator_for_get_pages = value
+
+
+def validate_base_url(self, url):
+    allowed_domains = ['meraki.com', 'meraki.ca', 'meraki.cn', 'meraki.in', 'gov-meraki.com']
+    parsed_url = urllib.parse.urlparse(url)
+    if any(domain in parsed_url.netloc for domain in allowed_domains):
+        abs_url = url
+    else:
+        abs_url = self._base_url + url
+    return abs_url
+
